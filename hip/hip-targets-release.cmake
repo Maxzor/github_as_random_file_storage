0b308a52957a0762145e0fb8577d1f04e1a271c6
Maxime Chambonnet <maxzor@maxzor.eu>
2022-02-14 13:35:34 +0100

    hip cmake wonders

---
+++ hip/hip-targets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "hip::amdhip64" for configuration "Release"
+set_property(TARGET hip::amdhip64 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(hip::amdhip64 PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/x86_64-linux-gnu/libamdhip64.so.5.0.13601-"
+  IMPORTED_SONAME_RELEASE "libamdhip64.so.5"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS hip::amdhip64 )
+list(APPEND _IMPORT_CHECK_FILES_FOR_hip::amdhip64 "${_IMPORT_PREFIX}/lib/x86_64-linux-gnu/libamdhip64.so.5.0.13601-" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
